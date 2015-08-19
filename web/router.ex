c4de0ed8166d6c46c9f014e1493c1cf836a18c97
jgarciasacristan <jose.garciasacristan@gmail.com>
2015-08-19 21:23:48 +0100

    prepare for sync with master

--- web/router.ex
+++ web/router.ex
@@ -16,6 +16,8 @@ defmodule EvercamMedia.Router do
     get "/v1/cameras/:id/live/snapshot", SnapshotController, :show
     post "/v1/cameras/:id/recordings/snapshots", SnapshotController, :create
 
+    # get "/onvif/cameras/:id/ptz/presets", ONVIFPTZController, :presets
+
     get "/live/:camera_id/index.m3u8", StreamController, :hls
     get "/live/:camera_id/:filename", StreamController, :ts
     get "/on_play", StreamController, :rtmp
