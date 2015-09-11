9b2f125db1798c1b731a399f9a7649d3e715f040
Miloš Mošić <mosic.milos@gmail.com>
2015-09-11 23:51:02 +0200

    Add runtime tools, log only errors in production

--- config/prod.exs
+++ config/prod.exs
@@ -26,7 +26,7 @@ config :evercam_media, EvercamMedia.Endpoint,
 # disk for the key and cert.
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :error
 
 # ## Using releases
 #
